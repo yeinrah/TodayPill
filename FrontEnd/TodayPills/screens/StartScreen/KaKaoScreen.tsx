4252bc951bdd65f9ca8c37f307410a4092feeb0b
kimjeongseo <wjdtj9656@gmail.com>
2022-10-25 17:23:02 +0900

    FEAT: create startPage

---
+++ FrontEnd/TodayPills/screens/StartScreen/KaKaoScreen.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native";
+import BackgroundScreen from "../BackgroundScreen";
+
+const KakaoScreen = () => {
+  return <BackgroundScreen></BackgroundScreen>;
+};
+export default KakaoScreen;
