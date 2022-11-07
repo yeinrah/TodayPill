fab0300f5ae7ec6887c9f3ccd680157e10b9129c
kimjeongseo <wjdtj9656@gmail.com>
2022-11-07 16:50:56 +0900

    FEAT:backlink update

--- FrontEnd/TodayPills/API/index.tsx
+++ FrontEnd/TodayPills/API/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-let URL = "http://10.0.2.2:8080/api";
-// let URL = "https://k7a706.p.ssafy.io/api";
+// let URL = "http://10.0.2.2:8080/api";
+let URL = "https://k7a706.p.ssafy.io/api";
 // if (window.location.hostname === "localhost") {
 //   URL = `http://localhost:8080/api`;
 // } else {
