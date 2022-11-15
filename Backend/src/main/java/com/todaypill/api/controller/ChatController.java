56a44bb7bd4a138df4d0dff6d7ebdfd604747513
kimyoungjin <khss4008@gmail.com>
2022-11-15 12:27:19 +0900

    FIX: ref

--- Backend/src/main/java/com/todaypill/api/controller/ChatController.java
+++ Backend/src/main/java/com/todaypill/api/controller/ChatController.java
@@ -1,188 +1,188 @@
-package com.todaypill.api.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.todaypill.chat.model.Message;
-import com.todaypill.chat.model.Status;
-import com.todaypill.db.entity.ChattingRoom;
-import com.todaypill.db.entity.Routine;
-import com.todaypill.service.ChatService;
-
-import io.swagger.annotations.ApiOperation;
-
-@CrossOrigin(origins = { "*" }, maxAge = 6000)
-@RestController
-@RequestMapping("/api")
-public class ChatController {
-
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
-    private ChatService chatService;
-    
-    
-    
-    public ChatController(SimpMessagingTemplate simpMessagingTemplate, ChatService chatService) {
-		super();
-		this.simpMessagingTemplate = simpMessagingTemplate;
-		this.chatService = chatService;
-	}
-    
-	@GetMapping("/getText/{roomName}")
-	@ApiOperation(value = "영양제 이름에 해당하는 채팅 방의 채팅 내용을 가져온다.", notes = "roomName 필요")
-	public ResponseEntity<?> getText(@PathVariable String roomName) throws Exception {
-		List<ChattingRoom> list = chatService.selectAllChat(roomName);
-		return new ResponseEntity<>(list, HttpStatus.OK);
-	}
-
-	@MessageMapping("/vitaminB")
-    @SendTo("/chatroom/vitaminB")
-    public Message receiveMessageVitaminB(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-		chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-				, message.getStatus(), message.getUser(), "vitaminB");
-        return message;
-    }
-
-    @MessageMapping("/vitaminC")
-    @SendTo("/chatroom/vitaminC")
-    public Message receiveMessageVitaminC(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "vitaminC");
-        return message;
-    }
-    @MessageMapping("/vitaminD")
-    @SendTo("/chatroom/vitaminD")
-    public Message receiveMessageVitaminD(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "vitaminD");
-    	return message;
-    }
-    @MessageMapping("/multivitamin")
-    @SendTo("/chatroom/multivitamin")
-    public Message receiveMessageMultivitamin(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "multivitamin");
-    	return message;
-    }
-    @MessageMapping("/magnesium")
-    @SendTo("/chatroom/magnesium")
-    public Message receiveMessageMagnesium(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "magnesium");
-    	return message;
-    }
-    @MessageMapping("/omega3")
-    @SendTo("/chatroom/omega3")
-    public Message receiveMessageOmega3(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "omega3");
-    	return message;
-    }
-    @MessageMapping("/milkthistle")
-    @SendTo("/chatroom/milkthistle")
-    public Message receiveMessageMilkthistle(@Payload Message message)throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "milkthistle");
-    	return message;
-    }
-    @MessageMapping("/lutein")
-    @SendTo("/chatroom/lutein")
-    public Message receiveMessageLutein(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "lutein");
-    	return message;
-    }
-    @MessageMapping("/zinc")
-    @SendTo("/chatroom/zinc")
-    public Message receiveMessageZinc(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "zinc");
-    	return message;
-    }
-    @MessageMapping("/lactobacillus")
-    @SendTo("/chatroom/lactobacillus")
-    public Message receiveMessageLactobacillus(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "lactobacillus");
-    	return message;
-    }
-    @MessageMapping("/collagen")
-    @SendTo("/chatroom/collagen")
-    public Message receiveMessageCollagen(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "collagen");
-    	return message;
-    }
-    @MessageMapping("/fe")
-    @SendTo("/chatroom/fe")
-    public Message receiveMessageFe(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "fe");
-    	return message;
-    }
-    @MessageMapping("/profolis")
-    @SendTo("/chatroom/profolis")
-    public Message receiveMessageProfolis(@Payload Message message) throws Exception{
-		if(message.getStatus()==Status.JOIN) ;
-		else if(message.getStatus()==Status.LEAVE);
-		else
-			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
-					, message.getStatus(), message.getUser(), "profolis");
-    	return message;
-    }
-//    @MessageMapping("/private-message")
-//    public Message recMessage(@Payload Message message){
-//        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message);
-//        System.out.println(message.toString());
+//package com.todaypill.api.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.messaging.handler.annotation.SendTo;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.todaypill.chat.model.Message;
+//import com.todaypill.chat.model.Status;
+//import com.todaypill.db.entity.ChattingRoom;
+//import com.todaypill.db.entity.Routine;
+//import com.todaypill.service.ChatService;
+//
+//import io.swagger.annotations.ApiOperation;
+//
+//@CrossOrigin(origins = { "*" }, maxAge = 6000)
+//@RestController
+//@RequestMapping("/api")
+//public class ChatController {
+//
+//    @Autowired
+//    private SimpMessagingTemplate simpMessagingTemplate;
+//    private ChatService chatService;
+//    
+//    
+//    
+//    public ChatController(SimpMessagingTemplate simpMessagingTemplate, ChatService chatService) {
+//		super();
+//		this.simpMessagingTemplate = simpMessagingTemplate;
+//		this.chatService = chatService;
+//	}
+//    
+//	@GetMapping("/getText/{roomName}")
+//	@ApiOperation(value = "영양제 이름에 해당하는 채팅 방의 채팅 내용을 가져온다.", notes = "roomName 필요")
+//	public ResponseEntity<?> getText(@PathVariable String roomName) throws Exception {
+//		List<ChattingRoom> list = chatService.selectAllChat(roomName);
+//		return new ResponseEntity<>(list, HttpStatus.OK);
+//	}
+//
+//	@MessageMapping("/vitaminB")
+//    @SendTo("/chatroom/vitaminB")
+//    public Message receiveMessageVitaminB(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//		chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//				, message.getStatus(), message.getUser(), "vitaminB");
 //        return message;
 //    }
-}
+//
+//    @MessageMapping("/vitaminC")
+//    @SendTo("/chatroom/vitaminC")
+//    public Message receiveMessageVitaminC(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "vitaminC");
+//        return message;
+//    }
+//    @MessageMapping("/vitaminD")
+//    @SendTo("/chatroom/vitaminD")
+//    public Message receiveMessageVitaminD(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "vitaminD");
+//    	return message;
+//    }
+//    @MessageMapping("/multivitamin")
+//    @SendTo("/chatroom/multivitamin")
+//    public Message receiveMessageMultivitamin(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "multivitamin");
+//    	return message;
+//    }
+//    @MessageMapping("/magnesium")
+//    @SendTo("/chatroom/magnesium")
+//    public Message receiveMessageMagnesium(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "magnesium");
+//    	return message;
+//    }
+//    @MessageMapping("/omega3")
+//    @SendTo("/chatroom/omega3")
+//    public Message receiveMessageOmega3(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "omega3");
+//    	return message;
+//    }
+//    @MessageMapping("/milkthistle")
+//    @SendTo("/chatroom/milkthistle")
+//    public Message receiveMessageMilkthistle(@Payload Message message)throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "milkthistle");
+//    	return message;
+//    }
+//    @MessageMapping("/lutein")
+//    @SendTo("/chatroom/lutein")
+//    public Message receiveMessageLutein(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "lutein");
+//    	return message;
+//    }
+//    @MessageMapping("/zinc")
+//    @SendTo("/chatroom/zinc")
+//    public Message receiveMessageZinc(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "zinc");
+//    	return message;
+//    }
+//    @MessageMapping("/lactobacillus")
+//    @SendTo("/chatroom/lactobacillus")
+//    public Message receiveMessageLactobacillus(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "lactobacillus");
+//    	return message;
+//    }
+//    @MessageMapping("/collagen")
+//    @SendTo("/chatroom/collagen")
+//    public Message receiveMessageCollagen(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "collagen");
+//    	return message;
+//    }
+//    @MessageMapping("/fe")
+//    @SendTo("/chatroom/fe")
+//    public Message receiveMessageFe(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "fe");
+//    	return message;
+//    }
+//    @MessageMapping("/profolis")
+//    @SendTo("/chatroom/profolis")
+//    public Message receiveMessageProfolis(@Payload Message message) throws Exception{
+//		if(message.getStatus()==Status.JOIN) ;
+//		else if(message.getStatus()==Status.LEAVE);
+//		else
+//			chatService.recordChat(message.get_id(),message.getText(), message.getSenderName(), message.getCreatedAt()
+//					, message.getStatus(), message.getUser(), "profolis");
+//    	return message;
+//    }
+////    @MessageMapping("/private-message")
+////    public Message recMessage(@Payload Message message){
+////        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message);
+////        System.out.println(message.toString());
+////        return message;
+////    }
+//}
--- Backend/src/main/java/com/todaypill/db/entity/ChattingRoom.java
+++ Backend/src/main/java/com/todaypill/db/entity/ChattingRoom.java
@@ -1,40 +1,46 @@
-package com.todaypill.db.entity;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import com.todaypill.chat.model.Status;
-import com.todaypill.chat.model.User;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Table(name = "chatting_room")
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class ChattingRoom {
-	
-	@Column(name = "_id")
-	private String _id;
-	@Column(name = "text")
-	private String text;
-	@Column(name = "senderName")
-    private String senderName;
-	@Column(name = "createdAt")
-    private String createdAt;
-	@Column(name = "status")
-    private Status status;
-	@Column(name = "user")
-    private User user;
-	@Column(name = "room_name")
-	private String roomName;
-}
+//package com.todaypill.db.entity;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//import com.todaypill.chat.model.Status;
+//import com.todaypill.chat.model.User;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Table(name = "chatting_room")
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class ChattingRoom {
+//	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@Column(name = "chat_id")
+//	private String chatId;
+//	
+//	@Column(name = "_id")
+//	private String _id;
+//	
+//	@Column(name = "text")
+//	private String text;
+//	@Column(name = "senderName")
+//    private String senderName;
+//	@Column(name = "createdAt")
+//    private String createdAt;
+////	@Column(name = "status")
+////    private Status status;
+////	@Column(name = "user")
+////    private User user;
+//	@Column(name = "room_name")
+//	private String roomName;
+//}
