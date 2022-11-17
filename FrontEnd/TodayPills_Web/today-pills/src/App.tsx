65626d7ca3a9debd1d1047476338cbca48e9a1bc
eunminbb <mimi0207ej@naver.com>
2022-11-17 17:38:43 +0900

    FEAT: make micro site

---
+++ FrontEnd/TodayPills_Web/today-pills/src/App.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import CustomBtn from "./CustomBtn";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <Header />
+      </div>
+      <img src={"/Assets/images/web-background.png"} alt="" className="back" />
+      <img
+        src={"/Assets/images/prototype2.png"}
+        alt=""
+        className="main-screen"
+      />
+
+      <div className="right-side">
+        <div>
+          <div className="intro">오직 나만을 위한</div>
+          <div className="intro">최적의 맞춤 영양제</div>
+        </div>
+        <div className="download-btn">
+          <img src={"/Assets/images/qrcode.png"} alt="" className="qrcode" />
+          <div className="btn-container">
+            <CustomBtn />
+          </div>
+        </div>
+      </div>
+
+      {/* <img
+      src={"/Assets/images/prototype3.png"}
+      alt=""
+      className="second-screen"
+    /> */}
+
+      {/* <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+     
+      
+    </header> */}
+    </div>
+  );
+}
+
+export default App;
